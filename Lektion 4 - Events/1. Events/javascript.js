45e2f5666e42e2ad72b51496092afc07b485b4f6
Micke Wessén <micke.wessen@hotmail.com>
2020-12-23 13:55:15 +0100

    lektion 4

---
+++ Lektion 4 - Events/1. Events/javascript.js
@@ -0,0 +1,61 @@
+/* 
+    Javascript Events
+    
+        Triggers: 
+        click, blur, focus, keydown, keyup, mouseclick dblclick etc.
+
+        document =  DOM - Document object model
+        window =    BOM - Browser object model
+*/
+
+
+// let counter = 0
+// const array = []
+
+// document.getElementById('input').addEventListener('keyup', (e) => {           //event förkortas e
+//     if(e.target.value.length < 4) {
+//         document.getElementById('results').innerText = 'För få tecken i ' +e.target.id
+//         document.getElementById('add_btn').disabled = true
+//     }
+
+//     else {
+//         document.getElementById('results').innerText = ''
+//         document.getElementById('add_btn').disabled = false
+//     }
+
+
+// })
+
+// document.getElementById('add_btn').addEventListener('click', () => {
+//     // console.log('clicked')
+//     // document.getElementById('results').innerText = 'klickat ' + ++counter + 'ggr.' 
+
+//     let element = document.getElementById('input')
+
+//     array.push(element.value)
+//     element.value = ""
+
+//     console.log(array)
+// })
+
+// document.getElementById('remove_btn').addEventListener('click', () => { 
+//     array.pop()
+//     console.log(array)
+// })
+
+document.getElementById('input').addEventListener('keyup', (e) => {
+    validateInputLength(e)
+})
+
+
+function validateInputLength(e) {
+    if(e.target.value.length < 4) {
+        document.getElementById('results').innerText = 'För få tecken i ' +e.target.id
+        document.getElementById('add_btn').disabled = true
+    }
+
+    else {
+        document.getElementById('results').innerText = ''
+        document.getElementById('add_btn').disabled = false
+    }
+}
