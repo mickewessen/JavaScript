3b2c046720baaab4230bb6e67100314e6c9ef5dd
Micke Wessén <micke.wessen@hotmail.com>
2020-12-28 16:09:34 +0100

    inlämningsuppgift 1

---
+++ Inlämningsuppgift 1/js/script.js
@@ -0,0 +1,6 @@
+// document.getElementById('firstname').addEventListener('keyup', (e) => {
+//     if(e.target.value.length < 4)
+//         document.getElementById('results').innerHTML =
+//     else
+//     document.getElementById('result').innerHTML = 
+// })
