05120d9fd505f05f49be4b55e85e61b3f420b33c
Micke Wessén <micke.wessen@hotmail.com>
2021-01-04 15:32:05 +0100

    lektion 5

---
+++ Lektion 5 - React/React/secondapp/src/app.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Welcome1 from './components/Welcome1'
+import Welcome2 from './components/Welcome2'
+import Welcome3 from './components/Welcome3'
+import Welcome4 from './components/Welcome4'
+import Alert from './components/Alert'
+import Navigation from './components/Navbar'
+
+
+
+
+export default function app() {
+    return (
+        <div>
+            <Navigation />
+            <Alert />
+            <Welcome1 />
+            <Welcome2 />
+            <Welcome3 />
+            <Welcome4 />
+        </div>
+    )
+}
---
+++ Lektion 5 - React/React/secondapp/src/components/Welcome2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Welcome2() {
+    return (
+      <div>Hej och Välkommen 2!</div>
+    )
+}
